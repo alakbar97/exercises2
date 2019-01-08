52747138d27876916066f635a8ab88254cd92cee
alakbar97 <alakbarsa@code.edu.az>
2019-01-09 01:04:51 +0400

    graph base created

---
+++ canvas/index.js
@@ -0,0 +1,104 @@
+var canvas=document.getElementById("canva");
+var ctx=canvas.getContext("2d");
+ctx.moveTo(40,40);
+ctx.lineTo(40,450);
+ctx.stroke();
+ctx.moveTo(30,440)
+ctx.lineTo(440,440);
+ctx.stroke();
+ctx.moveTo(30,40)
+ctx.lineTo(40,40);
+ctx.font = "15px Arial";
+ctx.fillText("100", 6, 45);
+ctx.stroke();
+ctx.moveTo(30,80)
+ctx.lineTo(40,80);
+ctx.font = "15px Arial";
+ctx.fillText("90", 10, 85);
+ctx.stroke();
+ctx.moveTo(30,120)
+ctx.lineTo(40,120);
+ctx.font = "15px Arial";
+ctx.fillText("80", 10, 125);
+ctx.stroke();
+ctx.moveTo(30,160)
+ctx.lineTo(40,160);
+ctx.font = "15px Arial";
+ctx.fillText("70", 10, 165);
+ctx.stroke();
+ctx.moveTo(30,200)
+ctx.lineTo(40,200);
+ctx.font = "15px Arial";
+ctx.fillText("60", 10, 205);
+ctx.stroke();
+ctx.moveTo(30,240)
+ctx.lineTo(40,240);
+ctx.font = "15px Arial";
+ctx.fillText("50", 10, 245);
+ctx.stroke();
+ctx.moveTo(30,280)
+ctx.lineTo(40,280);
+ctx.font = "15px Arial";
+ctx.fillText("40", 10, 285);
+ctx.stroke();
+ctx.moveTo(30,320)
+ctx.lineTo(40,320);
+ctx.font="15px Arial";
+ctx.fillText("30",10,325);
+ctx.stroke();
+ctx.moveTo(30,360)
+ctx.lineTo(40,360);
+ctx.font="15px Arial";
+ctx.fillText("20",10,365);
+ctx.stroke();
+ctx.moveTo(30,400)
+ctx.lineTo(40,400);
+ctx.font="15px Arial";
+ctx.fillText("10",10,405);
+ctx.stroke();
+ctx.moveTo(30,440)
+ctx.lineTo(40,440);
+ctx.font="15px Arial";
+ctx.fillText("0",10,445);
+ctx.stroke();
+
+
+
+ctx.moveTo(40,440)
+ctx.lineTo(40,450);
+ctx.stroke();
+ctx.moveTo(100,440)
+ctx.lineTo(100,450);
+ctx.font="15px Arial";
+ctx.fillText("JAN",50,465);
+ctx.stroke();
+ctx.moveTo(160,440)
+ctx.lineTo(160,450);
+ctx.font="15px Arial";
+ctx.fillText("FEB",120,465);
+ctx.stroke();
+ctx.moveTo(220,440)
+ctx.lineTo(220,450);
+ctx.font="15px Arial";
+ctx.fillText("MAR",180,465);
+ctx.stroke();
+ctx.moveTo(280,440)
+ctx.lineTo(280,450);
+ctx.font="15px Arial";
+ctx.fillText("APR",240,465);
+ctx.stroke();
+ctx.moveTo(340,440)
+ctx.lineTo(340,450);
+ctx.font="15px Arial";
+ctx.fillText("MAY",300,465);
+ctx.stroke();
+ctx.moveTo(400,440)
+ctx.lineTo(400,450);
+ctx.font="15px Arial";
+ctx.fillText("JUN",360,465);
+ctx.stroke();
+ctx.moveTo(440,440)
+ctx.lineTo(440,450);
+ctx.font="15px Arial";
+ctx.fillText("JUL",410,465);
+ctx.stroke();
